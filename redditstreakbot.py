13c3b2476d7365eefd1b72bde0fdc523ac9bd9ea
Rajtilak Bhattacharjee <rajtilak.blog@gmail.com>
2022-11-26 20:27:24 +0530

    Update redditstreakbot.py

--- redditstreakbot.py
+++ redditstreakbot.py
@@ -1,26 +1,42 @@
+import praw
+import os
 import requests
 import pandas as pd
-import praw
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 reddit = praw.Reddit(
-    client_id='',
-    client_secret='',
-    password='',
-    user_agent="",
-    username='',
+    client_id=os.getenv('API_CLIENT'),
+    client_secret=os.getenv('API_SECRET'),
+    password=os.getenv('REDDIT_PASSWORD'),
+    user_agent="Streak Bot for Reddit",
+    username=os.getenv('REDDIT_USERNAME'),
+    check_for_async=False
 )
 
-username = input("Enter your Reddit username: ")
+target_sub = "artofml"
+subreddit = reddit.subreddit(target_sub)
+trigger_phrase = "!streak"
 
-url = 'https://api.pushshift.io/reddit/comment/search?&limit=0&author=' + username + '&after=7d&metadata=true'
-data = requests.get(url)
-df = pd.json_normalize(data.json()['metadata'])
-dn = (df['total_results'])
-totalComments = dn[0]
+def getComments(username):
+    url = 'https://api.pushshift.io/reddit/comment/search?&limit=0&author=' + username + '&after=7d&metadata=true'
+    data = requests.get(url)
+    df = pd.json_normalize(data.json()['metadata'])
+    dn = (df['total_results'])
+    totalComments = dn[0]
+    return totalComments
 
-totalUpvotes = 0
-for comment in reddit.redditor(username).comments.new(limit=dn[0]):
-    totalUpvotes = totalUpvotes + (comment.score)
-    
-print("Your total comments for the past 7 days are: ", totalComments)
-print("Total upvotes on your comments for the past 7 days are: ", totalUpvotes)
+def getUpvotes(username,totalComments):
+    totalUpvotes = 0
+    for comment in reddit.redditor(username).comments.new(limit=totalComments):
+        totalUpvotes = totalUpvotes + (comment.score)
+    return totalUpvotes
+
+for comment in subreddit.stream.comments(skip_existing=True):  
+    if trigger_phrase in comment.body:  
+        author = str(comment.author)
+        totalComments = getComments(author)
+        totalUpvotes = getUpvotes(author,totalComments)
+        comment.reply("Total comments: " + str(totalComments) + " " + "Total upvotes: " + str(totalUpvotes))
